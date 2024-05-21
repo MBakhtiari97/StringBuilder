048513e1ddfe71d921cf70502b447dc304da4bfb
Mohammad Bakhtiari <mohammadba86@yahoo.com>
2024-05-21 19:47:01 +0330

    Add project files.

---
+++ WorkWithString/Program.cs
@@ -0,0 +1,33 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.Text;
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var summary = BenchmarkRunner.Run<StringBenchmarks>();
+    }
+}
+public class StringBenchmarks
+{
+    [Benchmark]
+    public string DoStringConcatenation()
+    {
+        string result = "";
+        for (int i = 0; i < 1000; i++)
+        {
+            result += i.ToString();
+        }
+        return result;
+    }
+    [Benchmark]
+    public string DoStringBuilderOperation()
+    {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < 1000; i++)
+        {
+            result.Append(i.ToString());
+        }
+        return result.ToString();
+    }
+}
