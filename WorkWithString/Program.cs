769c0d4606632809e121d5dea6465b11c0d1859c
Mohammad Bakhtiari <mohammadba86@yahoo.com>
2024-05-22 18:09:29 +0330

    Refactoring Classes

--- WorkWithString/Program.cs
+++ WorkWithString/Program.cs
@@ -1,33 +1,9 @@
-﻿using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Running;
-using System.Text;
+﻿using BenchmarkDotNet.Running;
+using WorkWithString;
 public static class Program
 {
     public static void Main(string[] args)
     {
         var summary = BenchmarkRunner.Run<StringBenchmarks>();
-    }
-}
-public class StringBenchmarks
-{
-    [Benchmark]
-    public string DoStringConcatenation()
-    {
-        string result = "";
-        for (int i = 0; i < 1000; i++)
-        {
-            result += i.ToString();
-        }
-        return result;
-    }
-    [Benchmark]
-    public string DoStringBuilderOperation()
-    {
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < 1000; i++)
-        {
-            result.Append(i.ToString());
-        }
-        return result.ToString();
     }
 }
---
+++ WorkWithString/StringBenchmarks.cs
@@ -0,0 +1,26 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Text;
+namespace WorkWithString;
+public class StringBenchmarks
+{
+    [Benchmark]
+    public string DoStringConcatenation()
+    {
+        string result = "";
+        for (int i = 0; i < 1000; i++)
+        {
+            result += i.ToString();
+        }
+        return result;
+    }
+    [Benchmark]
+    public string DoStringBuilderOperation()
+    {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < 1000; i++)
+        {
+            result.Append(i.ToString());
+        }
+        return result.ToString();
+    }
+}
